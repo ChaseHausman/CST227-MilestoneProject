457cc825a2b2eafecb50f2cb33017dfbfe6056dc
Chase Hausman <chase.hausman@comcast.net>
2019-02-18 17:33:43 -0800

    Bulk Commit

---
+++ Milestone 1/PlayerStats.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_1
+{
+    class PlayerStats : IComparable<PlayerStats>
+    {
+        public string Player { get; set; }
+        public int Level { get; set; }
+        public long Time { get; set; }
+
+        public PlayerStats(string Player, int Level, long Time)
+        {
+            this.Player = Player;
+            this.Level = Level;
+            this.Time = Time;
+        }
+
+        public int CompareTo(PlayerStats that)
+        {
+            if(this.Time < that.Time)
+            {
+                return -1;
+            } else if(this.Time > that.Time)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        public TimeSpan GetTimeSpan()
+        {
+            return TimeSpan.FromTicks(this.Time);
+        }
+    }
+}
---
+++ Milestone 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+
+            // We'll first prompt for the complexity level.
+            ComplexityForm complexity = new ComplexityForm();
+            complexity.ShowDialog();
+
+            int Size = complexity.GetSetting();
+
+            Console.WriteLine(Size);
+
+            MinesweeperGame Game = new MinesweeperGame(Size);
+
+            BoardForm boardForm = new BoardForm(Game);
+
+            Application.Run(boardForm);
+
+            //Game.playGame();
+        }
+    }
+}
